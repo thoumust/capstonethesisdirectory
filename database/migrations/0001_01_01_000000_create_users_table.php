aba1c5d3d4896ebc1787dad7e6e91e34935feb4f
Keane Lee <keanejoshua2003@gmail.com>
2024-09-24 19:13:30 +0800

    update seeders

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id(); #Student ID for students maybe Incremement for others
             $table->string('studentNumber')->nullable();
-            $table->string('name')->nullable(); #Temp
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();    
             $table->string('email')->unique();
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,42 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+
+
+        $users = [
+            ['first_name' => 'John', 'last_name'=> 'Doe', 'email' => 'john.doe@ust.edu.com', 
+            'user_type' => 'admin', 
+            'password' => bcrypt('Adminpassword123')],
+
+            ['first_name' => 'Jane', 'last_name'=> 'Doe', 'email' => 'jane.doe@ust.edu.com', 
+            'user_type' => 'faculty', 'user_course' => 'CS',
+            'password' => bcrypt('Facultypassword123')],
+            ['first_name' => 'Janet', 'last_name'=> 'Doe', 'email' => 'janet.doe@ust.edu.com', 
+            'user_type' => 'faculty', 'user_course' => 'IT',
+            'password' => bcrypt('Facultypassword123')],
+            ['first_name' => 'Janice', 'last_name'=> 'Doe', 'email' => 'janice.doe@ust.edu.com', 
+            'user_type' => 'faculty', 'user_course' => 'IS',
+            'password' => bcrypt('Facultypassword123')],
+
+            ['first_name' => 'Joe', 'last_name'=> 'Doe', 'email' => 'joe.doe.cics@ust.edu.com', 
+            'user_type' => 'student', 'user_course' => 'CS', 'studentNumber' => '2021000001',
+            'password' => bcrypt('Studentpassword123')],
+            ['first_name' => 'Joel', 'last_name'=> 'Doe', 'email' => 'joel.doe.cics@ust.edu.com', 
+            'user_type' => 'student', 'user_course' => 'IT', 'studentNumber' => '2021000002',
+            'password' => bcrypt('Studentpassword123')],
+            ['first_name' => 'Josh', 'last_name'=> 'Doe', 'email' => 'josh.doe.cics@ust.edu.com', 
+            'user_type' => 'student', 'user_course' => 'IS', 'studentNumber' => '2021000003',
+            'password' => bcrypt('Studentpassword123')],
+
+
+        ];
+
+        foreach ($users as $user) {
+            User::create($user);
+        }
+
+
+
+
     }
 }
