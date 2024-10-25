a6f415acc71d23442bdeee1cfc95cd188a9e26f7
thoumust <lesliefgsfuentes@gmail.com>
2024-10-25 16:15:14 +0800

    updated header: removed the logo. updated admin home layout. updated sidebar layout and remove collapsabel sidebar

--- resources/js/Pages/FacultyView/FacultySidebar.jsx
+++ resources/js/Pages/FacultyView/FacultySidebar.jsx
@@ -1,94 +1,83 @@
-
 import React, { useState } from "react";
-import "../../../css/StudentView/StudentProfile.css";
 import { Link } from '@inertiajs/react';
+import logo from '@/assets/logo.svg';
+import profile from '@/assets/profile.svg';
 
 const FacultySidebar = () => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [isIPDropdownOpen, setIsIPDropdownOpen] = useState(false);
-
 
   const toggleIPDropdown = () => {
     setIsIPDropdownOpen(!isIPDropdownOpen);
   };
 
-
   return (
-
-<nav className={`sidebar ${isSidebarOpen ? "open" : ""}`}>
-<ul>
-  <li>
-    <a href="/faculty-home" className="icon">
-      <img
-        src="/home.png"
-        alt="Home"
-        className="icon-image sb-icons"
-      />
-      <span className="label">Home</span>
-    </a>
-  </li>
-
-  
-
-  {/* IP Registered with Dropdown */}
-  <li>
-    <div className="icon" onClick={toggleIPDropdown} style={{ cursor: "pointer" }}>
-      <img
-        src="./manage.png"
-        alt="Manage"
-        className="icon-image sb-icons"
-      />
-      <span className="label">  IP registered</span>
-      <span className="dropdown-arrow">{isIPDropdownOpen ? "▲" : "▼"}</span>
-    </div>
-    {isIPDropdownOpen && (
-      <ul className="dropdown-list">
-        <li>
-          <Link href="/faculty/ip-registered/IT-cap" className="dropdown-item">IT Capstones</Link>
+    <nav className="bg-[#A4231C] text-white w-64 h-screen fixed top-0 left-0 flex flex-col justify-between">
+      <ul className="mt-6 space-y-4">
+        {/* Sidebar Header */}
+        <li className="flex justify-center mb-8">
+          <img src={logo} alt="Logo" className="w-12 h-12" />
         </li>
-        <li>
-          <Link href="/faculty/ip-registered/IS-cap" className="dropdown-item">IS Capstones</Link>
+
+        {/* Home Link */}
+        <li className="pl-6">
+          <a href="/faculty-home" className="flex items-center space-x-3 hover:bg-[#A62F2B] p-2 rounded-md">
+            <img src="/home.png" alt="Home" className="w-6 h-6" />
+            <span className="label">Home</span>
+          </a>
         </li>
-        <li>
-          <Link href="/faculty/ip-registered/CS-thes" className="dropdown-item">CS Thesis</Link>
+
+        {/* IP Registered with Dropdown */}
+        <li className="pl-6">
+          <div className="flex items-center space-x-3 cursor-pointer hover:bg-[#A62F2B] p-2 rounded-md" onClick={toggleIPDropdown}>
+            <img src="./manage.png" alt="Manage" className="w-6 h-6" />
+            <span className="label">IP Registered</span>
+            <span>{isIPDropdownOpen ? "▲" : "▼"}</span>
+          </div>
+          {isIPDropdownOpen && (
+            <ul className="ml-10 mt-2 space-y-2">
+              <li>
+                <Link href="/faculty/ip-registered/IT-cap" className="block hover:text-gray-200">IT Capstones</Link>
+              </li>
+              <li>
+                <Link href="/faculty/ip-registered/IS-cap" className="block hover:text-gray-200">IS Capstones</Link>
+              </li>
+              <li>
+                <Link href="/faculty/ip-registered/CS-thes" className="block hover:text-gray-200">CS Thesis</Link>
+              </li>
+            </ul>
+          )}
+        </li>
+
+        {/* Other Sidebar Items */}
+        <li className="pl-6">
+          <a href="/faculty/BestIT" className="flex items-center space-x-3 hover:bg-[#A62F2B] p-2 rounded-md">
+            <img src="./best.png" alt="IT" className="w-6 h-6" />
+            <span className="label">IT</span>
+          </a>
+        </li>
+        <li className="pl-6">
+          <a href="/faculty/BestIS" className="flex items-center space-x-3 hover:bg-[#A62F2B] p-2 rounded-md">
+            <img src="./best.png" alt="IS" className="w-6 h-6" />
+            <span className="label">IS</span>
+          </a>
+        </li>
+        <li className="pl-6">
+          <a href="/faculty/BestCS" className="flex items-center space-x-3 hover:bg-[#A62F2B] p-2 rounded-md">
+            <img src="./best.png" alt="CS" className="w-6 h-6" />
+            <span className="label">CS</span>
+          </a>
         </li>
       </ul>
-    )}
-  </li>
 
-  {/* Other Sidebar Items */}
-
-  <li>
-    <a href="/faculty/BestIT" className="icon">
-      <img src="./best.png" alt="IT" className="icon-image sb-icons" />
-      <span className="label">IT</span>
-    </a>
-  </li>
-  <li>
-    <a href="/faculty/BestIS" className="icon">
-      <img src="./best.png" alt="IS" className="icon-image sb-icons" />
-      <span className="label">IS</span>
-    </a>
-  </li>
-  <li>
-    <a href="/faculty/BestCS" className="icon">
-      <img src="./best.png" alt="CS" className="icon-image sb-icons" />
-      <span className="label">CS</span>
-    </a>
-  </li>
-</ul>
-<div className="profile-section">
-  <a href="/faculty/profile" className="profile-button">
-    <img
-      src="./profile.png"
-      alt="Profile"
-      className="icon-image sb-icons"
-    />
-    <span className="label">Profile</span>
-  </a>
-</div>
-</nav>
+      {/* Profile Section */}
+      <div className="pl-6 pb-4">
+        <a href="/faculty/profile" className="flex items-center space-x-3 hover:bg-[#A62F2B] p-8 rounded-md">
+          <img src={profile} alt="Profile" className="w-6 h-6" />
+          <span className="label">Profile</span>
+        </a>
+      </div>
+    </nav>
   );
 };
 
-export  default FacultySidebar;
+export default FacultySidebar;
--- resources/js/Pages/General/Header.jsx
+++ resources/js/Pages/General/Header.jsx
@@ -12,7 +12,6 @@ const Header = () => {
 
     return (
         <header className="flex items-center p-4 bg-[#A4231C] text-white sticky top-0 z-50">
-        <img src={logo} alt="Logo" className="w-12 mr-12 ml-8" />
         <div className="header-text">
         <h1>College of Information and Computing Sciences</h1>
         <h2>Capstone Projects and Thesis Papers Directory</h2>
